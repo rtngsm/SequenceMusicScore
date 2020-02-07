0cf815923acec5c289cd5fecea223423edecda19
rtngsm <rtnpku@pku.edu.cn>
2020-02-07 14:42:53 +0800

    Update get_wav_feature.py

--- get_wav_feature.py
+++ get_wav_feature.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Fri Jan 31 20:30:41 2020
-
+transfer mp3 to wav format and process wav file to get features
 @author: rtn
 """
 
@@ -10,13 +10,6 @@ from pydub import AudioSegment
 import tqdm
 import pandas as pd
 import numpy as np
-
-xml_classic_address = "D:\\xml\\classic"
-xml_folk_address = "D:\\xml\\folk"
-xml_jazz_address = "D:\\xml\\jazz"
-midi_classic_address = "D:\\MP3_data\\classic"
-midi_folk_address = "D:\\MP3_data\\folk"
-midi_jazz_address = "D:\\MP3_data\\jazz"
 
 
 wav_classic_address_22_16 = "D:\\MP3_data\\classic_wav_22_16\\"
@@ -40,26 +33,27 @@ mp3_classic_address = "D:\\MP3_data\\classic_mp3\\"
 mp3_jazz_address = "D:\\MP3_data\\jazz_mp3\\"
 mp3_folk_address = "D:\\MP3_data\\folk_mp3\\"
 
-### mp3 2 wav, 22050、44100采样率的wav文件，16bit，8bit的文件无法生成
+### mp3 to wav, 22050、44100 sample rate，16bit，32bit sample size
+
+##### chooose different setting to generate different wav file
 mp3_classic_list = os.listdir(mp3_classic_address)
 for m in mp3_classic_list:
-    sound = AudioSegment.from_mp3(mp3_jazz_address+m)
+    sound = AudioSegment.from_mp3(mp3_classic_address+m)
     name = m[:-4]
-    sound2 = sound.set_frame_rate(22050) ##采样率
+    sound2 = sound.set_frame_rate(22050) ##sample rate
     sound2 = sound2.set_channels(1)
-    sound2 = sound2.set_sample_width(4)  ###2等于16bit
+    sound2 = sound2.set_sample_width(4)  ###sample size,2 refres to 16bit,4 refers to 32 bit
     sound2.export(wav_classic_address_22_32+name+".wav", format="wav")
     sound3 = sound2.set_frame_rate(44100)
-    sound3.export(wav_jazz_address_44_32+name+".wav", format="wav")
+    sound3.export(wav_classic_address_44_32+name+".wav", format="wav")
     print(name)
 
 
-############################################提取特征，形成csv文件
+############################################extract audio features and save in .csv file
 from pyAudioAnalysis import audioBasicIO
 from pyAudioAnalysis import audioFeatureExtraction
-import matplotlib.pyplot as plt
 
-
+##### choose different address to get different .csv file according to wav format
 wav_classic_list = os.listdir(wav_classic_address_44_32)
 wav_jazz_list = os.listdir(wav_jazz_address_44_32)
 wav_folk_list = os.listdir(wav_folk_address_44_32)
@@ -127,12 +121,7 @@ for w in wav_folk_list:
 
 table.to_csv("D:\MP3_data\wav_data\\wav_feature_44_32.csv")
 
-[Fs, x] = audioBasicIO.readAudioFile("D:/MP3_data/classic_wav/01 Menuet22_8.wav")
-[Fs, x] = audioBasicIO.readAudioFile("D:\\MP3_data\\classic_wav_44_16\\01. Kyrie Eleison.wav")
-Fs.close
-x = x.astype(float)
-xx = x.sum(axis = 1)/2
-F = audioFeatureExtraction.stFeatureExtraction(xx, Fs, 0.050*Fs, 0.025*Fs);
+
 
 
 
